3df43757aab2366b6a264d34d0b36cfaf7e85671
dineshperam <pdreddy972@gmail.com>
2023-06-29 09:11:02 +0530

    updated..

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-const express = require('express')
-const app = express()
-const port = 5000
-const mongoDB =require('./db')
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 5000;
+const mongoDB = require('./db');
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://65.1.1.214:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors({
+  origin: '*'
+})); // Enable CORS for all origins
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+  res.send('Hello World!');
+});
 
-app.use(express.json())
-app.use("/api",require("./Routes/CreateUser"));
-app.use("/api",require("./Routes/DisplayData"));
-app.use("/api",require("./Routes/OrderData"));
-  app.listen(port, () => {
-    console.log(`Example app listening on http://localhost:${port}`)
-  })
+app.use(express.json());
+app.use('/api', require('./Routes/CreateUser'));
+app.use('/api', require('./Routes/DisplayData'));
+app.use('/api', require('./Routes/OrderData'));
+
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`);
+});
